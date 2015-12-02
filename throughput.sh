bf03bf6b57d07e9392d92755ebbb2ecbd7935c8a
ebrakus <brajeshk.119@gmail.com>
2015-12-02 11:53:25 +0000

    Script to measure throughput via netperf

---
+++ throughput.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+IP=167772164
+MESH_SIZE=$1
+PORT=5001
+
+ip2dec () {
+    local a b c d ip=$@
+    IFS=. read -r a b c d <<< "$ip"
+    printf '%d\n' "$((a * 256 ** 3 + b * 256 ** 2 + c * 256 + d))"
+}
+
+dec2ip () {
+    local ip dec=$@
+    for e in {3..0}
+    do
+        ((octet = dec / (256 ** e) ))
+        ((dec -= octet * 256 ** e))
+        ip+=$delim$octet
+        delim=.
+    done
+    printf '%s\n' "$ip"
+}
+
+netperf_temp () {
+    BW=$(netperf -l 60 -H $1 -p $2 -t TCP_STREAM -P 0 -v 0 -- -m 1436)
+    echo $3 $BW
+}
+
+SELF_IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+SELF_IP_N=$(ip2dec $SELF_IP)
+SELF_ID=$((SELF_IP_N-IP))
+
+for i in {0..9}
+do
+    if [ $i -eq $SELF_ID ]
+    then
+        echo Back
+        continue
+    fi
+    IP_ADD=$(dec2ip $((IP+i)))
+    netperf_temp $IP_ADD $PORT $i&
+done
